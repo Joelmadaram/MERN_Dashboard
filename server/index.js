afa084cf0e16e3072f8423f2888f08a80ce16115
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-09-16 13:27:29 -0500

    feat: Add routes and setup MongoDB connection

--- server/index.js
+++ server/index.js
@@ -7,3 +7,41 @@ app.use("/general", generalRoutes);
 app.use("/client", clientRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+
+import generalRoutes from "./routes/generalRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import managementRoutes from "./routes/managementRoutes.js";
+import salesRoutes from "./routes/salesRoutes.js";
+import mongoose from "mongoose";
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use("/general", generalRoutes);
+app.use("/client", clientRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
